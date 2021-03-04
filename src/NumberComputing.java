1ce95c812e7efb6b22586f05a808b1c0b8d13b07
Takeda Nana <40207168+windware1203@users.noreply.github.com>
2021-03-04 14:32:03 +0800

    Update NumberComputing.java

--- src/NumberComputing.java
+++ src/NumberComputing.java
@@ -6,11 +6,26 @@ public class NumberComputing
     public static void main(String[] args)
     {
         Scanner buffer = new Scanner(System.in);
+        int numCode;
+        numCode = buffer.nextInt();
+        switch (numCode)
+        {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+        }
+        /*
         int valueOne=0;
         int valueTwo=0;
         valueOne = buffer.nextInt();
         valueTwo = buffer.nextInt();
         Number numberOne = new Number(valueOne,valueTwo);
+        */
         buffer.close();
     }
 }
