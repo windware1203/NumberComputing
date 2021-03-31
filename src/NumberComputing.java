f233f05f9f2a3d225964e9199770154cbbeee7a7
Takeda Nana <40207168+windware1203@users.noreply.github.com>
2021-03-31 17:00:53 +0800

    up

--- src/NumberComputing.java
+++ src/NumberComputing.java
@@ -1,4 +1,4 @@
-import java.*;
+
 import java.util.Scanner;
 
 public class NumberComputing
@@ -6,41 +6,42 @@ public class NumberComputing
     public static void main(String[] args)
     {
         Scanner bufferOne = new Scanner(System.in);
-        String numCode;
-        System.out.println( "0:End\n"+
-                            "1:Odd/Even\n"+
-                            "2:GCD/LCM\n"+
-                            "3:Prime\n"+
-                            "enter the command code");
-        numCode = bufferOne.next();
-        while (numCode!="0")
+        int numCode;
+
+        boolean flag = true;
+        do
         {
-            switch (numCode)
-            {
-                case "":
-                    ParityCheck taskOne = new ParityCheck();
-                    System.out.println(taskOne.toString());
-                    break;
-                case "2":
-                    GcdLcm taskTwo = new GcdLcm();
-                    System.out.println(taskTwo.toString());
-                    break;
-                case "3":
-                    Eratosthenes taskThree = new Eratosthenes();
-                    System.out.println(taskThree.toString());
-                    break;
-            }
             System.out.println( "0:End\n"+
                     "1:Odd/Even\n"+
                     "2:GCD/LCM\n"+
                     "3:Prime\n"+
                     "enter the command code");
-            Scanner bufferTwo = new Scanner(System.in);
+            numCode = bufferOne.nextInt();
 
-            numCode = bufferTwo.next();
-            bufferTwo.close();
-        }
+            switch (numCode)
+            {
+                case 0:
+                    flag = false;
+                    break;
+                case 1:
+                    ParityCheck taskOne = new ParityCheck();
+                    System.out.println(taskOne.toString());
+                    break;
+                case 2:
+                    GcdLcm taskTwo = new GcdLcm();
+                    System.out.println(taskTwo.toString());
+                    break;
+                case 3:
+                    Eratosthenes taskThree = new Eratosthenes();
+                    System.out.println(taskThree.toString());
+                    break;
+                default:
+                    System.out.println("Error command code!\n" + "Please enter it again:");
+                    break;
+            }
+        }while (flag);bufferOne.close();
+
+
         System.out.println("----Thanks for using this system.\n"+"----Copyright\n----@Takeda Nana\n");
-        bufferOne.close();
     }
 }
